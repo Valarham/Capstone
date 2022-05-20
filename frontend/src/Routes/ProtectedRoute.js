82aee807601b5082e800e078b2154de88046986f
yousirong <80014277+yousirong@users.noreply.github.com>
2022-05-21 02:33:28 +0900

    20220521

--- frontend/src/Routes/ProtectedRoute.js
+++ frontend/src/Routes/ProtectedRoute.js
@@ -1,17 +1,37 @@
-// import { useSelector } from 'react-redux';
+// import { useState, useCallback, useEffect } from 'react';
 // import { Navigate } from 'react-router-dom';
 
 // const ProtectedRoute = ({ children, isAdmin }) => {
+//   const { loading, isAuthenticated, ping } = useState((state) => state.ping);
 
-//     const { loading, isAuthenticated, user } = useSelector(state => state.user);
-
-//     return (
-//         <>
-//             {loading === false && (
-//                 isAuthenticated === false ? <Navigate to="/login" /> : isAdmin ? user.role !== "admin" ? <Navigate to="/login" /> : children : children
-//             )}
-//         </>
-//     );
+//   return (
+//     <>
+//       {loading === false &&
+//         (isAuthenticated === false ? (
+//           <Navigate to="/login" />
+//         ) : isAdmin ? (
+//           ping.role !== 'dashboard' ? (
+//             <Navigate to="/login" />
+//           ) : (
+//             children
+//           )
+//         ) : (
+//           children
+//         ))}
+//     </>
+//   );
 // };
 
 // export default ProtectedRoute;
+import React from 'react';
+import { Route, Navigate } from 'react-router-dom';
+import isLogin from '../utils/isLogin';
+
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  return (
+    // Show the component only when the user is logged in
+    // Otherwise, redirect the user to /signin page
+    <Route {...rest} render={(props) => (isLogin() ? <Component {...props} /> : <Navigate to="/signin" />)} />
+  );
+};
+export default PrivateRoute;
